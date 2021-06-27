2235d933e78d9b330995896641e29bbe83ec1462
halifmuhammad <muhammad.halif@ui.ac.id>
2021-06-27 13:56:59 +0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Express');
-});
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/views/index.html');
+    });
+    app.use('/public', express.static(__dirname + '/public'))
+
 
 
 
