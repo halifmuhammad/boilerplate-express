9f4451647117ee882f46eab176a55d7d5457be31
halifmuhammad <muhammad.halif@ui.ac.id>
2021-06-27 19:04:46 +0700

    5/12

--- myApp.js
+++ myApp.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"})
-})
+    res.json({message: "Hello json"});
+});
 
 app.use('/public', express.static(__dirname + '/public'));
 
