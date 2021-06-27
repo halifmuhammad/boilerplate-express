7930a45703c0cc43ae8edbd857fe96c598967128
halifmuhammad <muhammad.halif@ui.ac.id>
2021-06-27 19:46:37 +0700

    7/12

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
